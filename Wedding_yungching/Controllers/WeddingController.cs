f8d5f13d90483c6c325812203e53636a7977a9e4
miss5you 謙 <miss5you@yahoo.com.tw>
2019-11-08 14:44:48 +0800

    新增登入頁面

---
+++ Wedding_yungching/Controllers/WeddingController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wedding_yungching.Controllers
+{
+    public class WeddingController : Controller
+    {
+        private SDpay userdb = new SDpay();
+        DateTime dt = DateTime.Now; // 取得現在時間
+        // GET: Wedding
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //管理者登入(新人)
+        [Authorize]
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string LoginAccount, string Password)
+        {
+
+            // User user = db.User.Where(x => x.LoginAccount == LoginAccount && x.Password == pwd).FirstOrDefault();
+            SDuser user = userdb.SDuser.Where(x => x.adaccount == LoginAccount && (x.state == 512 || x.state == 444)).FirstOrDefault();
+
+            if (user != null)
+            {
+                string pwd = UserDataHandler.Md5Hash(Password);
+                if (user.Password == pwd)
+                {
+                    UserDataHandler.LoginSaveToCookies(user);
+                    return RedirectToAction("NewPhotoWall", "Wedding");
+                }
+                else
+                {
+                    ViewBag.error = "帳號或密碼錯誤，請重新輸入。";
+                }
+            }
+            else
+            {
+                ViewBag.error = "帳號或密碼錯誤，請重新輸入。";
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ Wedding_yungching/Models/UserDataHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+
+namespace Wedding_yungching.Models
+{
+    public class UserDataHandler
+    {
+        //登入
+        public static void LoginSaveToCookies(SDuser user)
+        {
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(
+                version: 1,
+                name: user.adaccount,
+                issueDate: DateTime.Now,
+                expiration: DateTime.Now.AddHours(10),
+                isPersistent: false,
+                userData: user.name,//UserData用來儲存使用者編號
+                cookiePath: FormsAuthentication.FormsCookiePath
+                );
+
+            string encryptTicket = FormsAuthentication.Encrypt(ticket);
+            HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptTicket);
+            cookie.HttpOnly = true;
+            cookie.Expires = ticket.Expiration;
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+
+        public static string Md5Hash(string password)
+        {
+            MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+            byte[] data = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
